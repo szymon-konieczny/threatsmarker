874760d66ceb313d9578bf22b9264432b627d7f0
szymon-konieczny <szykon@gmail.com>
2019-11-18 01:36:51 +0100

    [#2] Update user endpoints list

--- server/src/features/users/index.ts
+++ server/src/features/users/index.ts
@@ -4,5 +4,9 @@ import { usersController } from './users.controller';
 const router = Router();
 
 router.get('/', usersController.getAll);
+router.get('/:id', usersController.getUser);
+router.post('/:id', usersController.addUser);
+router.put('/:id', usersController.updateUser);
+router.delete('/:id', usersController.removeUser);
 
 export default router;
--- server/src/features/users/users.controller.ts
+++ server/src/features/users/users.controller.ts
@@ -4,6 +4,22 @@ class UsersController {
   public getAll(req: Request, res: Response): void {
     res.json([1, 2, 3, 4, 5]);
   }
+
+  public getUser(req: Request, res: Response): void {
+    res.json('Get User');
+  }
+
+  public addUser(req: Request, res: Response): void {
+    res.json('Add User');
+  }
+
+  public updateUser(req: Request, res: Response): void {
+    res.json('Update User');
+  }
+
+  public removeUser(req: Request, res: Response): void {
+    res.json('Remove User');
+  }
 }
 
 export const usersController = new UsersController();
