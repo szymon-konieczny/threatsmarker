e9892ac7384d600943ae4f484f3f2ab7df964380
szymon-konieczny <szykon@gmail.com>
2019-12-21 15:37:25 +0100

    [#18] Refactor - server

--- server/src/features/users/users.controller.ts
+++ server/src/features/users/users.controller.ts
@@ -1,50 +1,51 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import { usersService } from './users.service';
 
 class UsersController {
-  public async getAll(req: Request, res: Response): Promise<void> {
+  public async getAll(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const data = await usersService.getUsers(req.query);
       res.json(data);
     } catch (err) {
-      throw new Error(err);
+      next(err);
     }
   }
 
-  public async getUser(req: Request, res: Response, next): Promise<void> {
+  public async getUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const user = await usersService.getUser(req.params.id);
       res.json(user);
     } catch (err) {
-      throw new Error(err);
+      next(err);
     }
   }
 
-  public async addUser(req: Request, res: Response): Promise<void> {
+  public async addUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const user = await usersService.addUser(req.body);
       res.json(user);
+      throw new Error('addUser error!!!!');
     } catch (err) {
-      throw new Error(err);
+      next(err);
     }
   }
 
-  public async updateUser(req: Request, res: Response): Promise<void> {
+  public async updateUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const user = await usersService.updateUser(req.body);
       res.json(user);
     } catch (err) {
-      throw new Error(err);
+      next(err);
     }
   }
 
-  public async removeUser(req: Request, res: Response): Promise<void> {
+  public async removeUser(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
       const user = await usersService.removeUser(req.params.id);
       res.json(user);
     } catch (err) {
-      throw new Error(err);
+      next(err);
     }
   }
 }
